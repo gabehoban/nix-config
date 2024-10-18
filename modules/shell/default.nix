d0a69805696c1de380128062cbfdb79a1c19debc
Gabe Hoban <gabehoban@icloud.com>
2024-10-17 22:03:29 -0400

    Update starship prompt

--- modules/shell/default.nix
+++ modules/shell/default.nix
@@ -99,96 +99,80 @@ in
           };
           starship = {
             enable = true;
-            settings = {
-              add_newline = true;
+            enableZshIntegration = true;
+            settings = lib.mkDefault {
               format = lib.concatStrings [
-                "($username )"
-                "($hostname )"
-                "$directory "
-                "($git_branch )"
-                "($git_commit )"
+                "$username"
+                "$hostname"
+                "$directory"
+                "$git_branch"
                 "$git_state"
                 "$git_status"
-                "\n$character"
+                "$nix_shell"
+                "$fill"
+                "$python"
+                "$golang"
+                "$status"
+                "$line_break"
+                "$character"
               ];
-              command_timeout = 60; # 60ms must be enough. I like a responsive prompt more than additional git information.
-              username = {
-                format = "[$user]($style)";
-                style_root = "bold red";
-                style_user = "bold purple";
-                aliases.root = "";
-              };
-              hostname = {
-                format = "[$hostname]($style)[$ssh_symbol](green)";
-                ssh_only = true;
-                ssh_symbol = " 󰣀";
-                style = "bold red";
-              };
-              directory = {
-                format = "[$path]($style)[$read_only]($read_only_style)";
-                fish_style_pwd_dir_length = 1;
-                style = "bold blue";
-              };
+
+              fill.symbol = " ";
+              hostname.ssh_symbol = "";
+              python.format = "([ $virtualenv]($style)) ";
+              rust.symbol = " ";
+              status.disabled = false;
+              username.format = "[$user]($style)@";
+
               character = {
-                success_symbol = "\\$";
-                error_symbol = "\\$";
-                vimcmd_symbol = "[](bold green)";
-                vimcmd_replace_one_symbol = "[](bold purple)";
-                vimcmd_replace_symbol = "[](bold purple)";
-                vimcmd_visual_symbol = "[](bold yellow)";
+                success_symbol = "[❯](purple)";
+                error_symbol = "[❯](red)";
+                vicmd_symbol = "[❯](green)";
               };
+
+              directory = {
+                read_only = " ";
+                home_symbol = " ~";
+                style = "blue";
+                truncate_to_repo = false;
+                truncation_length = 5;
+                truncation_symbol = ".../";
+              };
+
+              docker_context.symbol = " ";
+
               git_branch = {
-                format = "[$symbol$branch]($style)";
-                symbol = " ";
+                symbol = " ";
+                format = "[ $branch]($style) ";
                 style = "green";
               };
-              git_commit = {
-                commit_hash_length = 8;
-                format = "[$hash$tag]($style)";
-                style = "green";
-              };
+
               git_status = {
-                conflicted = "$count";
-                ahead = "⇡$count";
-                behind = "⇣$count";
-                diverged = "⇡$ahead_count⇣$behind_count";
-                untracked = "?$count";
-                stashed = "\\$$count";
-                modified = "!$count";
-                staged = "+$count";
-                renamed = "→$count";
-                deleted = "-$count";
-                format = lib.concatStrings [
-                  "[($conflicted )](red)"
-                  "[($stashed )](magenta)"
-                  "[($staged )](green)"
-                  "[($deleted )](red)"
-                  "[($renamed )](blue)"
-                  "[($modified )](yellow)"
-                  "[($untracked )](blue)"
-                  "[($ahead_behind )](green)"
-                ];
+                deleted = "✗";
+                modified = "✶";
+                staged = "✓";
+                stashed = "≡";
               };
-              status = {
+
+              git_state = {
+                format = "\([$state( $progress_current/$progress_total)]($style)\) ";
+                style = "bright-black";
+              };
+
+              golang = {
+                symbol = " ";
+                format = "[$symbol$version](cyan bold) ";
+              };
+
+              kubernetes = {
                 disabled = false;
-                pipestatus = true;
-                pipestatus_format = "$pipestatus => [$int( $signal_name)]($style)";
-                pipestatus_separator = "[|]($style)";
-                pipestatus_segment_format = "[$status]($style)";
-                format = "[$status( $signal_name)]($style)";
-                style = "red";
+                format = "[$symbol$context](cyan bold) ";
               };
-              python = {
-                format = "[$symbol$pyenv_prefix($version )(\($virtualenv\) )]($style)";
-              };
-              cmd_duration = {
-                format = "[ $duration]($style)";
-                style = "yellow";
-              };
-              time = {
-                format = "[ $time]($style)";
-                style = "cyan";
+
+              nix_shell = {
                 disabled = false;
+                symbol = "❄️ ";
+                format = "via [$symbol\($name\)]($style)";
               };
             };
           };
